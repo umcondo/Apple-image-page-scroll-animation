0fe9b2f3852a1d2aa5d26439aef993bb0367a49a
umcondo <superpowerkdg@gmail.com>
2022-04-10 22:30:28 +0900

    apple scroll image animation first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>interactive WEB</h1>
+<h2>애플 스크롤 이미지 애니메이션 무작정 따라해보기</h2>
+
+참고블로그
+[https://oddcode.tistory.com/155?category=878127](https://oddcode.tistory.com/155?category=878127)
---
+++ index.js
@@ -0,0 +1,5 @@
+const currentFrame = (index) => {
+  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+    .toString()
+    .padStart(4, "0")}.jpg`;
+};
